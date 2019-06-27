b6bd9cb4b30c11e58564267645a73a7f365b207a
J.R. Ruggiero <jrruggiero@shift3tech.com>
2019-06-27 11:02:36 -0700

    chore (main): routed boiler plate main, replaced placeholder

--- application/src/app/app-routing.module.ts
+++ application/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { MainComponent } from './main/main.component';
+
+const routes: Routes = [{
+  path: 'main',
+  component: MainComponent
+},
+{
+  path: '',
+  redirectTo: 'main',
+  pathMatch: 'full',
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
+
+export const routingComponents = [ MainComponent ];
