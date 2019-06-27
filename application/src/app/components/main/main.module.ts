1914b42834cb71c02059a47ae872896342acfdba
J.R. Ruggiero <jrruggiero@shift3tech.com>
2019-06-27 16:10:19 -0700

    feat(main): wired up runner list component, is responsive

--- application/src/app/components/main/main.module.ts
+++ application/src/app/components/main/main.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SegmentListComponent } from './segment-list/segment-list.component';
+import { RunnerListComponent } from './runner-list/runner-list.component';
 
 @NgModule({
     declarations: [
-        SegmentListComponent
+        SegmentListComponent, RunnerListComponent
     ],
     imports: [ CommonModule ],
     exports: [
-        SegmentListComponent
+        SegmentListComponent, RunnerListComponent
     ]
 })
 
---
+++ application/src/app/components/main/runner-list/runner-list.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'fe-comp-main-runner-list',
+    templateUrl: './runner-list.component.html',
+    styleUrls: [ './runner-list.component.scss' ]
+})
+
+export class RunnerListComponent implements OnInit {
+    ngOnInit() {
+
+    }
+}
