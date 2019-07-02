ad31281dd6157f78495b6e84660e226a6a942a2c
J.R. Ruggiero <jrruggiero@shift3tech.com>
2019-07-02 11:48:45 -0700

    fix(nav): nav burger works, replaced brand

--- application/src/app/components/nav/nav.component.ts
+++ application/src/app/components/nav/nav.component.ts
@@ -6,4 +6,11 @@ import { Component } from '@angular/core';
     styleUrls: [ './nav.component.scss' ]
 })
 
-export class NavComponent {}
+export class NavComponent {
+    burgerToggle() {
+        const burger = document.getElementById('burgerYum');
+        const menu = document.getElementById('navbarBasicExample');
+        burger.classList.toggle('is-active');
+        menu.classList.toggle('is-active');
+    }
+}
